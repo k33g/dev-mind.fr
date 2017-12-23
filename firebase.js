7a7eac32e5a6fc7bd367624e1eeb0788548501a1
k33g <ph.charriere@gmail.com>
2017-12-23 17:11:26 +0100

    try

---
+++ firebase.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "apiKey": process.env.API_KEY,
+  "authDomain": process.env.AUTH_DOMAIN,
+  "databaseURL": process.env.DATABASE_URL,
+  "storageBucket": process.env.STORAGE_BUCKET,
+  "user": process.env.USER,
+  "password": process.env.PASSWORD
+}
